a7bb76303dad3d01fe75f50decd3f847e78a07e2
jmsjcmc <jecemecojamestabilog@gmail.com>
2024-12-15 21:09:03 +0800

    first commit

---
+++ Api/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Api.Data;
+using Api.Models;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        
+    }
+}
---
+++ Api/DTO/UserDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.DTO
+{
+    public class UserDTO
+    {
+        public string Firstname { get; set; }
+        public string Middlename { get; set; }
+        public string Lastname { get; set; }
+        public string Role { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
